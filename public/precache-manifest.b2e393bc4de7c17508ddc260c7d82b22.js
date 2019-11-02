0316960f0c49c82db8192bfaff92b7916d17dffa
Mini <mini@Minis-Mac-mini.local>
2019-11-02 20:49:24 +0500

    fubusg

---
+++ public/precache-manifest.b2e393bc4de7c17508ddc260c7d82b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826ba9ebda137793d9cfcf6ebb0db186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6737a6f7475ca941738",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "085cef8c257b549ece98",
+    "url": "/static/js/2.8107a56c.chunk.js"
+  },
+  {
+    "revision": "c6737a6f7475ca941738",
+    "url": "/static/js/main.0612f3d7.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
--- server.js
+++ server.js
@@ -13,8 +13,11 @@ app.use(
   }),
 );
 
-
-const PORT = process.env.PORT || 7000;
+app.use(express.static('public'));
+app.get('*',(req,res,next)=>{
+    res.sendFile(path.resolve(__dirname,'public','index.html'))
+})
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server started on ${PORT}`)
 });
